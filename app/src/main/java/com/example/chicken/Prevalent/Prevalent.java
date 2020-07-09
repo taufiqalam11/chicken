5425235d335030e7441b5d834c751a1bcdbe9c28
Taufiq Alam <taufiqalam11>
2020-07-10 00:33:09 +0530

    Initial commit

---
+++ app/src/main/java/com/example/chicken/Prevalent/Prevalent.java
@@ -0,0 +1,11 @@
+package com.example.chicken.Prevalent;
+
+import Model.Users;
+
+public class Prevalent
+{
+    private static Users CurrentOnlineUser;
+
+    public static final String UserPhoneKey = "UserPhone";
+    public static final String UserPasswordKey = "UserPassword";
+}
